57daacc57ef2ea0d2425d29bac55196ad28a136d
AbdiMoalin23 <amn370@uregina.ca>
2026-09-05 15:14:50 -0600

    Fix Syntax Errors

--- App/models.py
+++ App/models.py
@@ -6,19 +6,19 @@ from sqlalchemy.orm import Mapped, mapped_column , relationship
 
 class User(Base):
     __tablename__ = "users"
-    id = Mapped[int] = mapped_column(Integer, primary_key=True)
-    email = Mapped[str] = mapped_column(String, unique=True)
-    hashed_password = Mapped[str] = mapped_column(String)
-    created_at = Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email:  Mapped[str] = mapped_column(String, unique=True)
+    hashed_password:  Mapped[str] = mapped_column(String)
+    created_at:  Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
 
-    notes = Mapped[List["Note"]] = relationship(back_populates="owner")
+    notes: Mapped[List["Note"]] = relationship(back_populates="owner")
 
 class Note(Base):
     __tablename__ = "notes"
-    title = Mapped[str] = mapped_column(String)
-    id = Mapped[int] = mapped_column(Integer, primary_key=True)
-    content = Mapped[str] = mapped_column(String)
-    favourite = Mapped[bool] = mapped_column(Boolean, default=False)
-    created_at = Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
-    owner_id = Mapped[int] = mapped_column(ForeignKey("users.id"))
+    title: Mapped[str] = mapped_column(String)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    content: Mapped[str] = mapped_column(String)
+    favourite: Mapped[bool] = mapped_column(Boolean, default=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    owner_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     owner: Mapped["User"] = relationship(back_populates="notes")
